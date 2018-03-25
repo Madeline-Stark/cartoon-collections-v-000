4e0b7a92d93af4955870ba7f51d6665f18e06249
Madeline Stark <madeline.stark1@gmail.com>
2018-03-25 19:54:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(munchies)
+def find_the_cheese(snacks)
   snacks.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
